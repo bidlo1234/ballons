3d8ba29fb044f2406f62eb28630db3d18c59ab2a
Evan <ikotskiy@gmail.com>
2025-03-20 19:59:54 +0700

    рамке

--- Form1.cs
+++ Form1.cs
@@ -26,14 +26,15 @@ namespace воздушные_шары
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             // Случайное количество окружностей
-            int count = rnd.Next(20);
-
+            int count = rnd.Next(1,20);
             for (int i = 0; i < count; i++)
             {
                 // Случайные параметры окружности
-                int radius = rnd.Next(100);
-                int x = rnd.Next(this.ClientSize.Width - radius);
-                int y = rnd.Next(this.ClientSize.Height - radius);
+                int radius = rnd.Next(200);
+                int minX = radius, minY = radius;
+
+                int x = rnd.Next(minX, this.ClientSize.Width - radius);
+                int y = rnd.Next(minY, this.ClientSize.Height - radius);
 
                 // Случайный цвет
                 Color color = Color.FromArgb(
@@ -42,11 +43,11 @@ namespace воздушные_шары
                     rnd.Next(256));
 
                 // Создаем кисть для заливки
-                using (SolidBrush brush = new SolidBrush(color))
-                {
-                    // Рисуем закрашенную окружность
-                    e.Graphics.FillEllipse(brush, x, y, 2 * radius, 2 * radius);
-                }
+                SolidBrush brush = new SolidBrush(color);
+                
+                // Рисуем закрашенную окружность
+                 e.Graphics.FillEllipse(brush, x, y, radius, radius);
+                
             }
         }
 
@@ -54,5 +55,6 @@ namespace воздушные_шары
         {
             this.Invalidate();
         }
+
     }
 }
